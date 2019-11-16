1b14124f72d5f1be55f2cd21bd6a09155a1d281e
Mingxi <3311386889@qq.com>
2019-11-16 20:15:53 +0800

    update controller and view

--- app/Controllers/Crossfire/Home.php
+++ app/Controllers/Crossfire/Home.php
@@ -17,28 +17,32 @@ class Home extends ResourcePresenter
         $linkedThing = anchor("/crossfire/home/show/".$record->id,"{$record->id}");
         $table->addRow($linkedThing, $record->name, $record->club); 
      }
-        //$parser = \Config\Services::parser();     
-        $view = \Config\Services::renderer();
-        $output = $view->render('crossfire/top').
-        //$parser->setData(['records' => $data])->render('content') .
-        $table->generate().
-        $view->render('crossfire/bottom');
+        $parser = \Config\Services::parser();     
+        $view = \Config\Services::renderer(); 
      
-        return $output;   
+        return
+        $view->render('crossfire/top').
+        //$parser->setData(['records' => $data])->render('content') .
+        //$table->generate().
+        $parser->setData(['table' => $table->generate()], 'raw')->render('crossfire/cfplayerList').
+        $view->render('crossfire/bottom');   
     }
     
-    public function show($id = NULL)
+    public function show($id = null)
     {
         //Retrieve one player data from Players Model with $id
         $cfplayers = new \App\Models\crossfire\Cfplayers();
         $record = $cfplayers->find($id);
         
-        // get a template parser
         $parser = \Config\Services::parser();
         
-        // tell it about the substitions
-        return 
+        return
+        $parser->render('crossfire/top').
         $parser->setData($record)->render('crossfire/oneCfplayer').
         $parser->render('crossfire/bottom');
+    }
+    public function edit($id = null) {
+        
+        parent::edit($id);
     }
 }
