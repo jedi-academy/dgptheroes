de5a93ce83aeb46d10275458cb29c46186195a63
Bincong <794759974@qq.com>
2019-11-19 10:00:54 +0800

    forget to commit

--- app/Controllers/movies/Home.php
+++ app/Controllers/movies/Home.php
@@ -6,30 +6,29 @@ class Home extends BaseController
 {
     public function index()
     {
-        //Retrieve all player data from Players Model
-        $starts = new \App\Models\movies\Start();
-        $records = $starts->findAll();
+
+        $start = new \App\Models\movies\Start();
+        $records = $start->findAll();
         
-        // get a template parser
-        $parser = \Config\Services::parser();
-        // tell it about the substitions
-        return $parser->setData(['records' => $records])
-            // and have it render the template with those
+        $start = \Config\Services::parser();
+        
+
+        return $start->setData(['records' => $records])
+
             ->render('movies/startList');
     }
     
     public function showme($id)
     {
-        //Retrieve one player data from Players Model with $id
-        $players = new \App\Models\movies\Start();
-        $record = $players->find($id);
-        
-        // get a template parser
+
+        $start = new \App\Models\movies\Start();
+        $record = $start->find($id);
+
         $parser = \Config\Services::parser();
         
-        // tell it about the substitions
+
         return $parser->setData($record)
-            // and have it render the template with those
+
             ->render('movies/star');
     }
 }
