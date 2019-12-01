397bfd37b1b9f4a620f6d3ddfc42909ba33ba3c8
Weida-Lin <374648344@qq.com>
2019-12-01 18:46:44 +0800

    Rules added

--- app/Models/hiphop/Musicians.php
+++ app/Models/hiphop/Musicians.php
@@ -7,5 +7,13 @@ class Musicians extends CSVModel
 {
     protected $origin = WRITEPATH . 'data/hiphop/musiciansData.csv';
     protected $keyField = 'id';
-    protected $validationRules = [];
+    protected $validationRules = [
+              'name' => 'required|min_length[2]',
+              'select' => 'required',
+              'birthdate' => 'required|valid_date',
+              'yearsactive' => 'required',
+              'labels' => 'required',
+              'instruments' => 'required',
+              'website' => 'required'
+    ];
 }
